d6bbbb29bcd92c5221e0a3532b9155c564ac70c4
hashitaku <s317062@ed.sus.ac.jp>
2018-06-23 13:46:09 +0900

    fix function "stack_push"
    関数のエラー分岐を追加

--- MyCLib/data_structure/stack/stack.c
+++ MyCLib/data_structure/stack/stack.c
@@ -21,8 +21,13 @@ int stack_empty(Stack *ptr_struct){
 }
 
 void stack_push(Stack *ptr_struct, int input){
-    ptr_struct->data[ptr_struct->top] = input;
-    ptr_struct->top++;
+    if(stack_full == 1){
+        printf("stack is full\n");
+        return;
+    }else{
+        ptr_struct->data[ptr_struct->top] = input;
+        ptr_struct->top++;
+    }
 }
 
 int stack_pop(Stack *ptr_struct){
