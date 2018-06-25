fb5751a39424919e658d2c1d31e1ac4cb9b97dbb
hashitaku <s317062@ed.sus.ac.jp>
2018-06-25 10:30:33 +0900

    fix funtion "stack_pop"
    printf関数の書式が間違っていたため変更
    (改行コードで全変更??)

--- MyCLib/data_structure/stack/stack.c
+++ MyCLib/data_structure/stack/stack.c
@@ -1,40 +1,40 @@
-#include"stack.h"
-
-void stack_init(Stack *ptr_struct){
-    ptr_struct->top = 0;
-}
-
-int stack_full(Stack *ptr_struct){
-    if(ptr_struct->top == STACK_DATA_MAX){
-        return 0;
-    }else{
-        return 1;
-    }
-}
-
-int stack_empty(Stack *ptr_struct){
-    if(ptr_struct->top == 0){
-        return 0;
-    }else{
-        return 1;
-    }
-}
-
-void stack_push(Stack *ptr_struct, int input){
-    if(stack_full(ptr_struct) == 0){
-        printf("stack is full\n");
-        return;
-    }else{
-        ptr_struct->data[ptr_struct->top] = input;
-        ptr_struct->top++;
-    }
-}
-
-int stack_pop(Stack *ptr_struct){
+#include"stack.h"
+
+void stack_init(Stack *ptr_struct){
+    ptr_struct->top = 0;
+}
+
+int stack_full(Stack *ptr_struct){
+    if(ptr_struct->top == STACK_DATA_MAX){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+int stack_empty(Stack *ptr_struct){
+    if(ptr_struct->top == 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+void stack_push(Stack *ptr_struct, int input){
+    if(stack_full(ptr_struct) == 0){
+        printf("stack is full\n");
+        return;
+    }else{
+        ptr_struct->data[ptr_struct->top] = input;
+        ptr_struct->top++;
+    }
+}
+
+int stack_pop(Stack *ptr_struct){
     if(stack_empty(ptr_struct) == 0){
-        printf("stack is empty\n", );
+        printf("stack is empty\n");
         return 1;
     }else{
         return ptr_struct->data[--ptr_struct->top];
     }
-}
+}
