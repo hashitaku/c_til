79d68b61137d252e6efb119c6ed0d77d11fbf950
hashitaku <s317062@ed.sus.ac.jp>
2018-11-28 16:56:11 +0900

    fix vector.c
    細かい修正
    typedef int Tで型をtypedefした

--- MyCLib/data_structure/vector/vector.c
+++ MyCLib/data_structure/vector/vector.c
@@ -4,9 +4,9 @@ int vector_init(Vector *ptr_struct){
     ptr_struct->data = NULL;
     ptr_struct->top = 0;
 
-    ptr_struct->data = (int*)malloc(sizeof(int));
+    ptr_struct->data = (T *)malloc(sizeof(T));
     if(ptr_struct->data == NULL){
-    	fprintf(stderr, "vector init error\n");
+    	fprintf(stderr, "vector_init error\n");
     	return EXIT_FAILURE;
     }else{
     	ptr_struct->size = 1;
@@ -15,10 +15,12 @@ int vector_init(Vector *ptr_struct){
 }
 
 int vector_free(Vector *ptr_struct){
-	if(ptr_struct->data == NULL){
+	if(ptr_struct->size == 0){
+        fprintf(stderr, "vector_free error\n");
 		return EXIT_FAILURE;
 	}else{
 		free(ptr_struct->data);
+        ptr_struct->size = 0;
 		return EXIT_SUCCESS;
 	}
 }
@@ -29,7 +31,7 @@ int vector_clear(Vector *ptr_struct){
 }
 
 int vector_resize(Vector *ptr_struct, size_t add_size){
-        ptr_struct->data = (int*)realloc(ptr_struct->data, sizeof(int) * (ptr_struct->size + add_size));
+        ptr_struct->data = (T *)realloc(ptr_struct->data, sizeof(T) * (ptr_struct->size + add_size));
         if(ptr_struct->data == NULL){
         	fprintf(stderr, "vector resize error\n");
         	return EXIT_FAILURE;
