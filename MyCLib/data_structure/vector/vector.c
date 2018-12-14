2d9390f68861364bbca46cc44999a221eba048e7
hashitaku <s317062@ed.sus.ac.jp>
2018-12-14 10:57:12 +0900

    add function vector_max_size
    キャパを返す関数を追加

--- MyCLib/data_structure/vector/vector.c
+++ MyCLib/data_structure/vector/vector.c
@@ -66,3 +66,7 @@ int vector_push_back(Vector *ptr_struct, int input){
 size_t vector_size(Vector *ptr_struct){
     return ptr_struct->size;
 }
+
+size_t vector_max_size(Vector *ptr_struct){
+    return ptr_struct->size;
+}
