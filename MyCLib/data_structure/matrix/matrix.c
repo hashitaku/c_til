6b4abd51ffc98a01d59136d38b712d7614ae1066
hashitaku <s317062@ed.sus.ac.jp>
2018-12-31 22:44:46 +0900

    add function ....
    行列関係の基本関数を追加

--- MyCLib/data_structure/matrix/matrix.c
+++ MyCLib/data_structure/matrix/matrix.c
@@ -1,2 +1,37 @@
 #include"./matrix.h"
 
+Matrix make_matrix(size_t row, size_t column){
+    Matrix retval;
+
+    retval.row = row;
+    retval.column = column;
+    retval.z = (double _Complex *)malloc(sizeof(double _Complex) * row * column);
+
+    if(retval.z == NULL){
+        fprintf(stderr, "make_matrix error\n");
+        exit(1);
+    }
+
+    return retval;
+}
+
+void free_matrix(Matrix *ptr_struct){
+    free(ptr_struct->z);
+}
+
+double _Complex *at_matrix(Matrix *ptr_struct, size_t n, size_t m){
+    if(ptr_struct->row - 1 < n || ptr_struct->column - 1 < m){
+        fprintf(stderr, "matrix out of range\n");
+        exit(1);
+    }
+
+    return &ptr_struct->z[ptr_struct->column * n + m];
+}
+
+void print_matrix(Matrix *ptr_struct){
+    for(size_t i = 0; i < ptr_struct->row; i++){
+        for(size_t j = 0; j < ptr_struct->column; j++){
+            printf("%g%+gi  ", creal(*at_matrix(ptr_struct, i, j)), cimag(*at_matrix(ptr_struct, i, j)));
+        }printf("\n");
+    }
+}
