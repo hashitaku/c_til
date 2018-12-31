f4c3be38e1a1178d1201ed61f842457cc11e25f5
hashitaku <s317062@ed.sus.ac.jp>
2018-12-31 14:20:19 +0900

    fix function main
    収束誤差用のグラフも追加

--- MyCMachineLearnig/main.c
+++ MyCMachineLearnig/main.c
@@ -6,9 +6,11 @@
 #include"./core.h"
 
 const double eta = 0.1; //学習率
-const size_t epoch = 10; //学習回数
+// const size_t epoch = 10; //学習回数
+enum {epoch = 10};
+double convergence_error_array[epoch];
 const double convergence_error = 0.0001;
-double w[3]; //重みベクトル
+double w[3] = {[2] = 1.0}; //重みベクトル
 
 double f(double, double, double);
 
@@ -26,8 +28,14 @@ int main(){
     data_input(data_file, &data);
 
     FILE *pipe = popen("gnuplot -persist", "w");
+    FILE *pipe2 = popen("gnuplot -persist", "w");
 
     if(pipe == NULL){
+        fprintf(stderr, "popen error\n");
+        exit(1);
+    }
+
+    if(pipe2 == NULL){
         fprintf(stderr, "popen error\n");
         exit(1);
     }
@@ -38,6 +46,11 @@ int main(){
     fprintf(pipe, "set ylabel 'math points'\n");
     // fprintf(pipe, "set terminal gif animate optimize delay 13 size 600, 600\n"); //gifへ出力 必要ないならコメントアウト
     // fprintf(pipe, "set output 'tmp.gif'\n"); //gifへ出力 必要ないならコメントアウト
+
+    fprintf(pipe2, "set xrange [-1:%d]\n",epoch);
+    fprintf(pipe2, "set yrange [-1:100]\n");
+    fprintf(pipe2, "set xlabel 'epoch'\n");
+    fprintf(pipe2, "set ylabel 'convergence_error'\n");
 
     for(size_t i = 0; i < epoch; i++){
         double rmse = 0.0;
@@ -50,6 +63,15 @@ int main(){
         }
 
         rmse = sqrt(rmse / count_line(data_file));
+
+        convergence_error_array[i] = rmse;
+
+        fprintf(pipe2, "plot '-' with points\n");
+        for(size_t j = 0; j <= i; j++){
+            fprintf(pipe2, "%zd %f\n", j, convergence_error_array[j]);
+        }fprintf(pipe2, "e\n");
+
+        fflush(pipe2);
 
         printf("%f\n", rmse);
         if(rmse < convergence_error) break;
@@ -79,6 +101,7 @@ int main(){
 
     fclose(data_file);
     pclose(pipe);
+    pclose(pipe2);
     free(data.english);
     free(data.math);
     free(data.flag);
