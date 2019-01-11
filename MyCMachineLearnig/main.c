08d8324698077b011f2142f369e38d6268c2875f
hashitaku <s317062@ed.sus.ac.jp>
2019-01-11 13:34:22 +0900

    fix function main
    コメントの追加

--- MyCMachineLearnig/main.c
+++ MyCMachineLearnig/main.c
@@ -9,10 +9,10 @@ const double eta = 0.1; //学習率
 // const size_t epoch = 10; //学習回数
 enum {epoch = 10}; //配列の添字にしたいのでconst intはNG
 double convergence_error_array[epoch];
-const double convergence_error = 0.0001;
+const double convergence_error = 0.0001; //誤差の最低値
 double w[3] = {[2] = 1.0}; //重みベクトル
 
-double f(double, double, double);
+double f(double, double, double); //rmse用なくてもいいが見にくいので実装
 
 int main(){
     
@@ -40,8 +40,8 @@ int main(){
         exit(1);
     }
 
-    fprintf(pipe, "set xrange [-75:75]\n");
-    fprintf(pipe, "set yrange [-75:75]\n");
+    fprintf(pipe, "set xrange [-75:75]\n"); //描画範囲は任意
+    fprintf(pipe, "set yrange [-75:75]\n"); //描画範囲は任意
     fprintf(pipe, "set xlabel 'english points'\n");
     fprintf(pipe, "set ylabel 'math points'\n");
     // fprintf(pipe, "set terminal gif animate optimize delay 13 size 600, 600\n"); //gifへ出力 必要ないならコメントアウト
@@ -55,7 +55,7 @@ int main(){
     double ave_math = 0.0; //中心化のためのデータの平均値
     double ave_english = 0.0; //中心化のためのデータの平均値
 
-    for(size_t i = 0; i < count_line(data_file); i++){
+    for(size_t i = 0; i < count_line(data_file); i++){ //平均値を取るためにデータの合計を取得
         ave_english += data.english[i];
         ave_math += data.math[i];
     }
@@ -63,15 +63,15 @@ int main(){
     ave_english /= count_line(data_file); //平均
     ave_math /= count_line(data_file); //平均
 
-    for(size_t i = 0; i < count_line(data_file); i++){
+    for(size_t i = 0; i < count_line(data_file); i++){ //中心化処理
         data.english[i] -= ave_english;
         data.math[i] -= ave_math;
     }
 
-    for(size_t i = 0; i < epoch; i++){
-        double rmse = 0.0;
+    for(size_t i = 0; i < epoch; i++){ //学習階数分forを回す
+        double rmse = 0.0; //エポックごとに誤差を出す
 
-        for(size_t j = 0; j < count_line(data_file); j++){
+        for(size_t j = 0; j < count_line(data_file); j++){ //誤差(rmse)の計算
             int error = data.flag[j] - step(w[0], w[1], w[2], data.english[j], data.math[j]);
             if(error != 0){
                 rmse += pow(data.math[j] - f(-(w[0] / w[1]), -(w[2] / w[1]), data.english[j]), 2.0);
@@ -80,19 +80,19 @@ int main(){
 
         rmse = sqrt(rmse / count_line(data_file));
 
-        convergence_error_array[i] = rmse;
+        convergence_error_array[i] = rmse; //gnuplotにプロットするために誤差を配列へ代入
 
-        fprintf(pipe2, "plot '-' with points\n");
+        fprintf(pipe2, "plot '-' with points\n"); //誤差用のプロット
         for(size_t j = 0; j <= i; j++){
             fprintf(pipe2, "%zd %f\n", j, convergence_error_array[j]);
         }fprintf(pipe2, "e\n");
 
-        fflush(pipe2);
+        fflush(pipe2); //フラッシュしないとpcloseのタイミングで描画されるので
 
         printf("%f\n", rmse);
-        if(rmse < convergence_error) break;
+        if(rmse < convergence_error) break; //誤差が一定に達したらbreakで終了
 
-        for(size_t j = 0; j < count_line(data_file); j++){
+        for(size_t j = 0; j < count_line(data_file); j++){ //パッチ勾配降下法
             int error = data.flag[j] - step(w[0], w[1], w[2], data.english[j], data.math[j]);
             if(error != 0){
                 w[0] += eta * error * data.english[j];
