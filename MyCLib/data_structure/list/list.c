6b3802b606d1b04a21e0e889e7f6b437cc60c863
hashitaku <s317062@ed.sus.ac.jp>
2019-06-12 02:15:48 +0900

    add data_structure list
    双方向リストを追加

--- MyCLib/data_structure/list/list.c
+++ MyCLib/data_structure/list/list.c
@@ -1,3 +1,92 @@
 #include"list.h"
 
+int list_init(List *ptr_struct){
+    if(ptr_struct == NULL){
+        fprintf(stderr, "list_init arg nullptr\n");
+        exit(1);
+    }
+
+    ptr_struct->begin = NULL;
+    ptr_struct->end = NULL;
+    ptr_struct->size = 0;
+
+    return EXIT_SUCCESS;
+}
+
+void list_free(List *ptr_struct){
+    while(ptr_struct->begin != NULL){
+        struct List_impl *next_node = ptr_struct->begin->next;
+        free(ptr_struct->begin);
+        ptr_struct->begin = next_node;
+    }
+}
+
+void list_push_back(List *ptr_struct, T input){
+    if(ptr_struct == NULL){
+        fprintf(stderr, "list_push_back arg nullptr\n");
+        exit(1);
+    }
+
+    struct List_impl *new_node = malloc(sizeof(struct List_impl));
+
+    if(new_node == NULL){
+        fprintf(stderr, "list_push_back malloc error\n");
+        exit(1);
+    }
+
+    if(ptr_struct->size == 0){
+        new_node->data = input;
+        new_node->next = NULL;
+        new_node->prev = NULL;
+        ptr_struct->end = new_node;
+        ptr_struct->begin = new_node;
+    }else{
+        new_node->data = input;
+        new_node->next = NULL;
+        new_node->prev = ptr_struct->end;
+        ptr_struct->end->next = new_node;
+        ptr_struct->end = new_node;
+    }
+
+    ptr_struct->size++;
+}
+
+void list_push_front(List *ptr_struct, T input){
+    if(ptr_struct == NULL){
+        fprintf(stderr, "list_push_front arg nullptr\n");
+        exit(1);
+    }
+
+    struct List_impl *new_node = malloc(sizeof(struct List_impl));
+
+    if(new_node == NULL){
+        fprintf(stderr, "list_push_front malloc error\n");
+        exit(1);
+    }
+
+    if(ptr_struct->size == 0){
+        new_node->data = input;
+        new_node->next = NULL;
+        new_node->prev = NULL;
+        ptr_struct->end = new_node;
+        ptr_struct->begin = new_node;
+    }else{
+        new_node->data = input;
+        new_node->prev = NULL;
+        new_node->next = ptr_struct->begin;
+        ptr_struct->begin->prev = new_node;
+        ptr_struct->begin = new_node;
+    }
+
+    ptr_struct->size++;
+}
+
+void list_print(const List *ptr_struct){
+    struct List_impl *node = ptr_struct->begin;
+    while(node != NULL){
+        printf("%d\n", node->data);
+        node = node->next;
+    }
+}
+
 
--- MyCLib/data_structure/list/list.h
+++ MyCLib/data_structure/list/list.h
@@ -1,6 +1,29 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include<stdio.h>
 #include<stdlib.h>
 
-#endif LIST_H
+typedef int T;
+
+struct List_impl{
+    T data;
+    struct List_impl *prev;
+    struct List_impl *next; 
+};
+
+typedef struct{
+    struct List_impl *begin;
+    struct List_impl *end;
+    size_t size;
+}List;
+
+int list_init(List *ptr_struct);
+void list_free(List *ptr_struct);
+
+void list_push_back(List *ptr_struct, T input);
+void list_push_front(List *ptr_struct, T input);
+
+void list_print(const List *ptr_struct);
+
+#endif //LIST_H
